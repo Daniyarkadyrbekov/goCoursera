0ca81a9973a89101ef785ba14951dbb55dc79961
Daniyarkadyrbekov <d.kadyrbekov@mail.ru>
2018-07-25 00:53:12 +0300

    dfas

--- week2/hw2_signer/signer.go
+++ week2/hw2_signer/signer.go
@@ -8,6 +8,19 @@ import (
 // сюда писать код
 func ExecutePipeline(jobs... job){
 	fmt.Print("jobs len ", len(jobs), "\n")
+
+	var chans []chan interface{}
+	chans = make([]chan interface{}, len(jobs) + 1)
+	for i := range  chans{
+		chans[i] = make(chan interface{})
+	}
+	for i, curJob := range jobs{
+		if i != len(jobs) - 1{
+			go curJob(chans[i], chans[i + 1])
+		}else{
+			go curJob(chans[i], chans[i + 1])
+		}
+	}
 }
 
 func SingleHash(in, out chan interface{}){
@@ -23,17 +36,28 @@ func SingleHash(in, out chan interface{}){
 
 func MultiHash(in, out chan interface{}){
 	input := <- in
+	inputString, ok := input.(string)
+	if !ok {
+		fmt.Errorf("Unable to convert to steing in MultiHash")
+	}
+	result := ""
 	for i := 0; i < 6; i++{
 		stringOfIteration := strconv.Itoa(i)
-		inputString, ok := input.(string)
-		if !ok {
-			fmt.Errorf("Unable to convert to steing in MultiHash")
-		}
-		result := DataSignerCrc32(stringOfIteration + inputString)
-		out <- result
+		result = result + DataSignerCrc32(stringOfIteration + inputString)
 	}
+	//fmt.Printf("result ")
+	out <- result
 }
 
 func CombineResults(in, out chan interface{}){
-
+	result := ""
+	for {
+		select{
+		case <-in:
+			resultChan := <-in
+			result = resultChan.(string) + "_"
+		}
+	}
+	out <- result
+	close(out)
 }
